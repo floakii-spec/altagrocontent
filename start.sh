78a109a6c1f4874ed652e02c81923677ddc7b20e
Pedro Henrique Faustino Medeiros <floakii@MacBook-Air-de-Pedro.local>
2026-04-15 01:18:18 -0300

    chore: remove debug lines from start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@ set -e
 
 export PYTHONPATH=/app:$PYTHONPATH
 
-echo "DATABASE_URL is set: ${DATABASE_URL:+yes}"
-echo "DATABASE_URL prefix: ${DATABASE_URL:0:20}"
 echo "Running database migrations..."
 alembic upgrade head
 
